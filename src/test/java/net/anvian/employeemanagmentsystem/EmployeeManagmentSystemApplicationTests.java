ab886af8aec47fb8971e5e0acbb38a97c7c593da
andre-carbajal <carbajalvargasandre@gmail.com>
2024-03-21 18:20:03 -0500

    first commit

---
+++ src/test/java/net/anvian/employeemanagmentsystem/EmployeeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anvian.employeemanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
