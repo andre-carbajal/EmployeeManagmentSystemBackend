a875018d8f2fd03a6a7950d09a6f7bb3c55d0cc1
andre-carbajal <carbajalvargasandre@gmail.com>
2024-03-22 18:27:32 -0500

    create employees

--- src/main/java/net/anvian/employeemanagmentsystem/controller/EmployeeController.java
+++ src/main/java/net/anvian/employeemanagmentsystem/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package net.anvian.employeemanagmentsystem.controller;
 import net.anvian.employeemanagmentsystem.model.Employee;
 import net.anvian.employeemanagmentsystem.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
